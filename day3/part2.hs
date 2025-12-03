6cfd932b64e835f4dabc9ace60d67cf7898ef66e
sg1825 <sg1825@ic.ac.uk>
2025-12-03 13:44:47 +0000

    done part 3

--- day3/part2.hs
+++ day3/part2.hs
@@ -4,14 +4,19 @@ import Data.List (sortOn)
 maxList :: (Ord a, Num a) => [a] -> (a, Int)
 maxList xs = head $ sortOn (negate.fst) (zip xs [0..])
 
-largestDouble :: [Int] -> Int
-largestDouble xs = 10 * ae + be
-    where 
-        (ae, ai) = maxList (init xs)
-        (be, _) = maxList $ drop (ai + 1) xs
+dropBack :: Int -> [a] -> [a]
+dropBack n xs = take (length xs - n) xs
+
+largestn :: [Int] -> Int -> [Int]
+largestn xs 1 = [fst $ maxList xs]
+largestn xs n = ae : largestn (drop (ai + 1) xs) (n - 1)
+    where (ae, ai) = maxList $ dropBack (n - 1) xs
+
+sintToInt :: [Int] -> Int
+sintToInt xs = sum $ zipWith (*) (reverse xs) (iterate (*10) 1)
 
 solve :: [String] -> Int
-solve xs = sum $ [largestDouble (map digitToInt x) | x <- xs]
+solve xs = sum $ [sintToInt $ largestn (map digitToInt x) 12 | x <- xs]
 
 main :: IO()
 main = do 
